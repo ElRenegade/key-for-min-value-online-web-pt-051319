518db52233d957c88f03b565cafd76f063d52f99
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-24 19:59:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lvalue = 0
   lkey = nil
-  name_hash.each { |key, value| 
+  name_hash.each { |key, value|
       if value == 0 || value < lvalue
         lvalue = value
         lkey = key
